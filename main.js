63163c52e7f473a1d10c2ef2b499f9d944311b12
Ben Patterson <bmizepatterson@gmail.com>
2018-10-09 13:12:19 -0400

    Calculate other text statistics

--- main.js
+++ main.js
@@ -8,33 +8,50 @@ new Vue({
 
     computed: {
         paragraphNum: function() {
-            return this.count(/.\n/g);
+            if (this.input) {
+                return this.count(/.\n/g) + 1;
+            }
+            return 0;
         },
 
         sentenceNum: function() {
-            return 0;
+            // TODO: Improperly matches abbreviations.
+            return this.count(/\s+[A-Za-z,;'\"\s]+[.?!]/gm);
         },
 
         wordNum: function() {
-            return 0;
+            return this.count(/[\w']+/g);
         },
 
         charNum: function() {
-            return 0;
+            return this.count(/./g);
+        },
+
+        letterNum: function() {
+            return this.count(/[A-Za-z]/g);
         },
 
         longestWord: function() {
-            return 'Triskaedekaphobia';
-        }
+            let words = this.search(/[\w']+/g);
+            let lengths = words.map((word) => word.length);
+            let longest = lengths.indexOf(Math.max(...lengths));
+            return words[longest];
+        },
+
+
     },
 
     methods: {
-        update: function(event) {
+        update: _.debounce(function(event) {
             this.input = event.target.value;
-        },
+        }, 300),
 
         count: function(regex) {
-            return ((this.input || '').match(regex) || []).length;
+            return this.search(regex).length;
+        },
+
+        search: function(regex) {
+            return (this.input || '').match(regex) || [];
         }
     }
 });
